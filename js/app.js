529fe76a0e33874747f20d537790312c6f514ee4
Samrood Ali <devsamrood@gmail.com>
2020-06-15 16:54:15 +0400

    FEAT: Smooth Scroll

--- js/app.js
+++ js/app.js
@@ -34,20 +34,56 @@
 // let numSection be th number of sections
 let numSection = 1;
 const navList = document.getElementById('navbar__list');
+const section = document.getElementById('section1');
+const mainBody = document.getElementById('main_body');
+
+//function to create new sections
+function sectionCreater() {
+    const sectionClone = section.cloneNode(true);
+    sectionClone.setAttribute('id', `section${numSection}`);
+    mainBody.appendChild(sectionClone);
+}
 // build the nav
-function newSection() {
+function sectionTabCreater() {
     const newA = document.createElement('a');
     const newli = document.createElement("li");
     newA.href = `#section${numSection}`;
-    newli.textContent = `section${numSection}`;
+    newli.textContent = `section ${numSection}`;
     numSection++;
     newA.appendChild(newli);
     navList.appendChild(newA);
+    // Scroll to anchor ID using scrollTO event
+    newA.addEventListener(
+        'click',
+        function(event) {
+            event.preventDefault();
+            const targetId = event.currentTarget.getAttribute("href");
+            let top = document.querySelector(targetId).offsetTop - 100;
+            window.scrollTo({
+                top: top,
+                behavior: "smooth",
+            })
+        }
+    );
 }
+//initial number of sections.
+sectionTabCreater();
+sectionTabCreater();
+sectionTabCreater();
+
+//functionality for new section.
+let newSectionTab = document.getElementById('newSection');
+newSectionTab.addEventListener(
+    "click",
+    function newSection() {
+        sectionCreater();
+        sectionTabCreater();
+    }
+);
 
 // Add class 'active' to section when near top of viewport
 
-// Scroll to anchor ID using scrollTO event
+
 
 /**
  * End Main Functions
@@ -58,5 +94,6 @@ function newSection() {
 // Build menu 
 
 // Scroll to section on link click
+
 
 // Set sections as active
